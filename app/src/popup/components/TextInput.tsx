606ef3ca938894042ec722d6799115c92248858b
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-02-18 14:19:12 +0100

    Apply new line length to TextInput

--- app/src/popup/components/TextInput.tsx
+++ app/src/popup/components/TextInput.tsx
@@ -3,7 +3,12 @@ import React from "react";
 export const TextInput = () => {
     return (
         <div className="tab-search-entry-list-container">
-            <input className="tab-search-entry-list" id="tab-search-entry-list-id" placeholder="..." type="text" />
+            <input
+                className="tab-search-entry-list"
+                id="tab-search-entry-list-id"
+                placeholder="..."
+                type="text"
+            />
         </div>
     );
 };
