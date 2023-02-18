0c542164ee866792b1d195e0add795ade67cd0f9
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-02-18 14:11:58 +0100

    Implement basic text input for search text field

---
+++ app/src/popup/components/TextInput.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const TextInput = () => {
+    return (
+        <div className="tab-search-entry-list-container">
+            <input className="tab-search-entry-list" id="tab-search-entry-list-id" placeholder="..." type="text" />
+        </div>
+    );
+};
