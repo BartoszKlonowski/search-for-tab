1db577d371f902b405b99b46cce3f5b7dd0d7ba8
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-02-19 23:23:54 +0100

    Provide TabResultsList with memoization for search phrase

--- app/src/popup/components/TabResultsList.tsx
+++ app/src/popup/components/TabResultsList.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useMemo, useState} from "react";
+import Browser from "webextension-polyfill";
 
 type Props = {
     tabSearchPhrase: string;
-}
+};
 
 export const TabResultsList = (props: Props) => {
-    return (
-        <div className="tab-search-results-list-container"></div>
+    const {tabSearchPhrase} = props;
+    const [allTabs, setAllTabs] = useState<Browser.Tabs.Tab[]>();
+
+    useMemo(
+        () =>
+            Browser.tabs
+                .query({currentWindow: true})
+                .then((tabs) => {
+                    setAllTabs(
+                        tabs.filter(
+                            (tab) =>
+                                tab.title?.includes(tabSearchPhrase) ||
+                                tab.url?.includes(tabSearchPhrase)
+                        )
+                    );
+                })
+                .catch((error) => {
+                    console.error(error);
+                }),
+        [tabSearchPhrase]
     );
-}
+
+    return <div className="tab-search-results-list-container"></div>;
+};
