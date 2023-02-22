30e8b342606c379c3030e94407d0dd2db021a8a1
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-02-22 22:43:29 +0100

    Redesign the structure of TabResultTile component

--- app/src/popup/components/TabResultTile.tsx
+++ app/src/popup/components/TabResultTile.tsx
@@ -5,13 +5,25 @@ type Props = {
 };
 
 export const TabResultTile = (props: Props) => {
+    const {tabTitle} = props;
+
+    const trimTitleText = (): string => {
+        const maxTitleTextLength = 60;
+        return tabTitle.length > 60 ? tabTitle.slice(0, maxTitleTextLength).concat("...") : tabTitle;
+    };
+
     return (
-        <div>
-            <div className="tab-tile-icon-container"></div>
-            <div className="tab-tile-title-container">{props.tabTitle}</div>
-            <div className="tab-tile-actions-container">
-                <div className="tab-tile-goto-action-icon-container"></div>
-                <div className="tab-tile-close-action-icon-container"></div>
+        <div className="tab-tile-container">
+            <div className="tab-tile-icon-container">
+                <img/>
+            </div>
+            <div className="tab-tile-title-container">
+                <div className="tab-tile-title">
+                    {trimTitleText()}
+                </div>
+            </div>
+            <div className="tab-tile-close-action-icon-container">
+                <div>{'+'}</div>
             </div>
         </div>
     );
