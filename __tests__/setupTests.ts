c13633e45d3c1457d2f4a5236bf88e4554c356f3
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-02-24 01:33:18 +0100

    Fix setup of translations tests

--- __tests__/setupTests.ts
+++ __tests__/setupTests.ts
@@ -3,6 +3,7 @@ if (!global.chrome.runtime) global.chrome.runtime = {};
 if (!global.chrome.runtime.id) global.chrome.runtime.id = "testid";
 if (!global.browser) global.browser = {tabs: {}};
 if (!global.browser.i18n) global.browser.i18n = {};
+if (!global.browser.i18n.getUILanguage) global.browser.i18n.getUILanguage = () => "EN";
 
 class LocalStorageMock {
     getItem(key) {
